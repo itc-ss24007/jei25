d27e6de1dbfc5acd8ab82a9f053d7df92855106a
itc-ss24007 <s24007@std.it-college.ac.jp>
2025-06-12 11:40:07 +0900

    課題

---
+++ D50Q25.java
@@ -0,0 +1,9 @@
+public class D50Q25 {
+    public static void main(String[] args) {
+        for(int i = 0;i <=8;i++){
+            if(i % 2 !=0)
+                continue;
+            System.out.print(i);
+        }
+    }
+}
