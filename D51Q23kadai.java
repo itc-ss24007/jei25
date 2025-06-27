c4619a734e2304ac6984109c45f7ff57cdd9da07
itc-ss24007 <s24007@std.it-college.ac.jp>
2025-06-27 12:26:29 +0900

    課題

---
+++ D51Q23kadai.java
@@ -0,0 +1,9 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] seasons ={"春","夏","秋","冬"};
+        for (String season : seasons) {
+            System.out.print(season+"。");
+        }
+
+    }
+}
